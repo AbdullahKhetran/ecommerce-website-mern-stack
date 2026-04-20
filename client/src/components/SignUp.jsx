3c384df8c5519a2233aee2a91bbacb60e197089d
Abdullah Khetran <khetranabdullah@gmail.com>
2026-04-21 01:17:13 +0500

    sign up page

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,8 +1,61 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import TextInput from "./TextInput";
+import Button from "./Button"
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 500px;
+  display: flex:
+  flex-direction: column;
+  gap: 36px;  
+`;
+
+const Title = styled.div`
+  font-size: 30px;
+  font-weight: 800;
+  color: ${({theme}) => theme.primary};
+`;
+
+const Span = styled.div`
+  font-size: 16px;
+  font-weight: 400;
+  color: ${({theme}) => theme.text_secondary + 90};
+`;
 
 function SignUp() {
   return (
-    <div>SignUp</div>
+    <Container>
+      <div>
+        <Title>Create New Account</Title>
+        <Span>Please enter details to create a new account</Span>
+      </div>
+
+      <div 
+        style={{
+          display: "flex", 
+          gap: "20px", 
+          flexDirection: "column"
+        }} 
+      >
+        
+        {/* Fields: name, email and password */}
+        <TextInput 
+          label="Full Name"
+          placeholder="Enter your full name"
+        />
+        <TextInput 
+          label="Email Address"
+          placeholder="Enter your email address"
+        />
+        <TextInput 
+          label="Password"
+          placeholder="Enter your password"
+        />
+                
+        <Button text="Sign Up" />
+      </div>
+    </Container>
   )
 }
 
