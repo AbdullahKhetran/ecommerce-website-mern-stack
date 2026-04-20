e55439493224527caca763f003b7b31da946c15c
Abdullah Khetran <khetranabdullah@gmail.com>
2026-04-20 05:47:01 +0500

    navbar placeholder added

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,33 @@
+import styled, { ThemeProvider } from "styled-components"
+import {lightTheme} from "./utils/Themes"
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import Navbar from "./components/Navbar";
+
+// stlyed componend with custom css
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background: ${({theme}) => theme.bg};
+  color: ${({theme}) => theme.text_primary};
+  overflow-x: hidden;
+  overflow-y: hidden;
+  transition: all 0.2s ease;
+`;
+
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <ThemeProvider theme={lightTheme}>
+      <BrowserRouter>
+        <Container>
+          <Navbar>
+
+          </Navbar>
+        </Container>
+      </BrowserRouter>      
+        
+    </ThemeProvider>
   );
 }
 
