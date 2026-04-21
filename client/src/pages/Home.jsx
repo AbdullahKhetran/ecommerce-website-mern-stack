c6ff0422ae3aac58c93815f3847da8b93646aa61
Abdullah Khetran <khetranabdullah@gmail.com>
2026-04-21 22:39:48 +0500

    homepage banner and categories

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,83 @@
 import React from 'react'
+import styled from "styled-components"
+import HeaderImage from "../utils/Images/Header.png"
+import {category} from "../utils/data"
+import ProductCategoryCard from '../components/cards/ProductCategoryCard'
+
+const Container = styled.div`
+  padding: 20px 30px;
+  padding-bottom: 200px;
+  height: 100%;
+  overflow-y: scroll;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 30px;
+  @media (max-width: 768px) {
+    padding: 20px 12px;
+  }
+  background: ${({theme}) => theme.bg};
+`;
+
+const Section = styled.div`
+  max-width: 1400px;
+  padding: 32px 16px;
+  display: flex;
+  flex-direction: column;
+  gap: 28px;
+`;
+
+const Img = styled.img`
+  width: 90%;
+  height: 700px;
+  object-fit: cover;
+  max-width: 1200px;
+`;
+
+const Title = styled.div`
+  font-size: 28px;
+  font-weight: 500;
+  display: flex;
+  justify-content: ${({center}) => (center ? "center" : "space-between")};
+  align-items: center;
+`
+
+const CardWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 24px;
+  justify-content: center;
+`
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Container>
+
+      {/* background image section */}
+      <Section
+        style={{
+          alignItems: "center"
+        }}
+      >
+        <Img src={HeaderImage} />
+      </Section>
+
+      {/* categories */}
+      <Section >
+        <Title>Shop by Categories</Title>
+        <CardWrapper>
+          {category.map((category) => (
+            <ProductCategoryCard category={category} />
+            
+          ))}
+        </CardWrapper>
+      </Section>
+
+      {/* bestsellers */}
+      <Section >
+        <Title center>Our Bestseller</Title>
+      </Section>
+    </Container>
   )
 }
 
