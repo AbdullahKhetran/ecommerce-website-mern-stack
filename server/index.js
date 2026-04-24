0d3d0fdfe023f88024311e957e5c1f393b8a35d6
Abdullah Khetran <khetranabdullah@gmail.com>
2026-04-25 04:53:20 +0500

    fixed typo

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({extended: true}));
 
 // error handle
 app.use((err,req,res,next) => {
-    const status = err.stats || 500;
+    const status = err.status || 500;
     const message = err.message || "Something went wrong";
     return res.status(status).json({
         success: false,
