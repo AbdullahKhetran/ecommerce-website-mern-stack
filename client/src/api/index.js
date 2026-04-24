0257dd4a39154cfb7f77b42bfb59fa468c33dfb9
Abdullah Khetran <khetranabdullah@gmail.com>
2026-04-24 23:53:06 +0500

    added api functions in frontend

---
+++ client/src/api/index.js
@@ -0,0 +1,80 @@
+import axios from "axios";
+
+const API = axios.create({
+    baseURL: "https://localhost:8080/api/"
+})
+
+
+// auth apis
+export const UserSignUp = async (data) => {
+    return await API.post("/user/signup", data)
+}
+
+export const UserSignIn = async (data) => {
+    return await API.post("/user/signin", data)
+}
+
+
+// products apis
+export const getAllProducts = async (filter) => {
+    return await API.get(`/products?${filter}`)
+}
+
+export const getProductDetails = async (id) => {
+    return await API.get(`/products/${id}`)
+}
+
+
+// cart apis
+export const getCart = async (token) => {
+    return await API.get("/user/cart", {
+        headers: { Authorization: `Bearer ${token}`},
+    })
+}
+
+export const addToCart = async (token, data) => {
+    return await API.post("/user/cart", data, {
+        headers: { Authorization: `Bearer ${token}`},
+    })
+}
+
+export const deleteFromCart = async (token, data) => {
+    return await API.patch("/user/cart", data, {
+        headers: { Authorization: `Bearer ${token}`},
+    })
+}
+
+
+
+// favorites apis
+export const getFavorite = async (token) => {
+    return await API.get("/user/favorites", {
+        headers: { Authorization: `Bearer ${token}`},
+    })
+}
+
+export const addToFavorite = async (token, data) => {
+    return await API.post("/user/favorites", data, {
+        headers: { Authorization: `Bearer ${token}`},
+    })
+}
+
+export const deleteFromFavorite = async (token, data) => {
+    return await API.patch("/user/favorites", data, {
+        headers: { Authorization: `Bearer ${token}`},
+    })
+}
+
+
+// orders apis
+export const getOrders = async (token) => {
+    return await API.get("/user/order/", {
+        headers: {Authorization: `Bearer ${token}`}
+    })
+}
+
+export const placeOrder = async (token, data) => {
+    return await API.post("/user/order/", data, {
+        headers: {Authorization: `Bearer ${token}`}
+    })
+}
