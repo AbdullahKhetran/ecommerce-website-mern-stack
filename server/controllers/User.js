0f425a6953673245589a7905f4397db910128fe0
Abdullah Khetran <khetranabdullah@gmail.com>
2026-04-24 23:01:26 +0500

    user apis - cart, order, favorites

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -66,3 +66,185 @@ export const UserLogin = async(req,res,next) => {
 };
 
 
+
+// add to cart
+export const addToCart = async(req,res,next) => {
+    try {
+        const {productId, quantity} = req.body;
+        const userJWT = req.user;
+        const user = await User.findById(userJWT.id);
+
+        const existingCartItemIndex = user.cart.findIndex((item) => {
+            item?.product?.equals(productId)
+        });
+
+        if (existingCartItemIndex !== -1) {
+            user.cart[existingCartItemIndex].quantity += quantity
+        } else {
+            user.cart.push({product: productId, quanitity});
+        }
+        await user.save();
+
+        return res
+        .status(200)
+        .json({message: "Product added to cart successfully", user});
+        
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+// remove from cart
+export const removeFromCart = async (req,res,next) => {
+    try {
+        const {productId, quantity} = req.body;
+        const userJWT = req.user;
+        const user = await User.findById(userJWT.id);
+
+        if (!user) {
+            return next(createError(404, "User not found"))
+        }
+        const productIndex = user.cart.findIndex((item) => {
+            item.product.equals(productId)
+        })
+        if (productIndex !== -1) {
+            if (quanitity && qunatity > 0) {
+                user.cart[productIndex].quantity -= quantity;
+                // if after decrementing it goes neagtive, remove product from cart
+                if (user.cart[productIndex].quantity <= 0) {
+                    user.cart.splice(productIndex, 1)
+                }
+            } else {
+                user.cart.splice(productIndex, 1)
+            }
+
+            await user.save();
+            return res
+            .status(200)
+            .json({message: "Product quantity updated in cart", user})
+        }
+        else {
+            return next(createError(404, "Product not found in the user's cart"))
+        }
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+// get cart items
+export const getAllCartItems =async (req,res,next) => {
+    try {
+        const userJWT = req.user;
+        const user = await User.findById(userJWT.id).populate({
+            path: "cart.product",
+            model: "Products",
+        });
+
+        const cartItems = user.cart;
+        return res.status(200).json(cartItems)
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+// place order
+export const placeOrder = async (req,res,next) => {
+    try {
+        const {prodcuts, address, totalAmount} = req.body;
+        const userJWT = req.user;
+        const user = await User.findById(userJWT.id);
+        
+        const order = new Orders({
+            products,
+            user: user._id,
+            total_amount: totalAmount,
+            address,
+        })
+        await order.save();
+
+        user.cart.save();
+
+        user.cart = [];
+        await user.save();
+
+        return res
+        .status(200)
+        .json({message: "Order placed successfully", order})
+        
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+// get all orders
+export const getAllOrders = async (req,res,next) => {
+    try {
+        const user = req.user;
+        const orders = await Orders.find({user: user.id});
+        return res.status(200).json(orders);
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+// add to favorites
+export const addToFavorites = async (req,res,next) => {
+    try {
+        const {productId} = req.body;
+        const userJWT = req.user;
+        const user = await User.findById(userJWT.id);
+
+        if (!user.favorites.includes(productId)) {
+            user.favorites.push(productId);
+            await user.save()
+        }
+        
+        return res
+        .status(200)
+        .json({message: "Product added to favorites successfully", user})
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+// remove from favorites
+export const removeFromFavorites = async (req,res,next) => {
+    try {
+        const {productId} = req.body;
+        const userJWT = req.user;
+        const user = await User.findById(userJWT.id);
+
+        user.favorites = user.favorites.filter((fav) => !fav.equals(productId));
+        await user.save();
+        return res
+        .status(200)
+        .json({message: "Product removed from favorites successfully", user});
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+// get user favorites
+export const getUserFavorites = async (req,res,next) => {
+    try {
+        const userId = req.user.id;
+        const user = await User.findById(userId).populate("favorites").exec();
+
+        if (!user) {
+            return next(createError(404, "User not found"))
+        }
+
+        return res.status(200).json(user.favorites)
+
+    } catch (error) {
+        next(error)
+    }
+}
--- server/routes/User.js
+++ server/routes/User.js
@@ -1,9 +1,25 @@
 import express from "express";
-import { UserRegister, UserLogin } from "../controllers/User.js";
+import { UserRegister, UserLogin, getAllCartItems, addToCart, removeFromCart, getAllOrders, placeOrder, getUserFavorites, addToFavorites, removeFromFavorites } from "../controllers/User.js";
 
 const router = express.Router();
 
+// auth
 router.post("/signup", UserRegister);
 router.post("/signin", UserLogin);
+
+// cart
+router.get("/cart", getAllCartItems);
+router.post("/cart", addToCart);
+router.patch("/cart", removeFromCart);
+
+// order
+router.get("/order", getAllOrders);
+router.post("/order", placeOrder);
+
+// favorites
+router.get("/favorite", getUserFavorites);
+router.post("/favorite", addToFavorites);
+router.patch("/favorite", removeFromFavorites)
+
 
 export default router;
