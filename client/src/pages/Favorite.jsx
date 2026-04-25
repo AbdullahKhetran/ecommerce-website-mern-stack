b8d706411981db0b0963cdb65d94e5e5fcea2894
Abdullah Khetran <khetranabdullah@gmail.com>
2026-04-25 11:00:38 +0500

    integreated backend and redux into favorite page

--- client/src/pages/Favorite.jsx
+++ client/src/pages/Favorite.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components';
 import ProductCard from '../components/cards/ProductCard';
+import { getFavorite } from '../api';
+import { CircularProgress } from '@mui/material';
 
 const Container = styled.div`
   padding: 20px 30px;
@@ -51,16 +53,44 @@ const CardWrapper = styled.div`
 `
 
 const Favorite = () => {
+  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [reload, setReload] = useState(false);
+
+  const getProducts = async () => {
+    setLoading(true);
+    const token = localStorage.getItem("krist-app-token");
+    await getFavorite(token).then((res) => {
+      setProducts(res.data);
+      setLoading(false);
+      setReload(!reload)
+    })
+  }
+
+  useEffect(() => {
+    getProducts()
+  }, [])
+
   return (
     <Container>
       <Section>
         <Title>Your favorites</Title>
         <CardWrapper>
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
+          {loading ? (
+            <CircularProgress />
+          ) : (
+            <>
+              {products.length === 0 ? (
+                <>No Products</>
+              ) : (
+                <CardWrapper>
+                  {products.map((product) => (
+                    <ProductCard product={product} />
+                  ))}
+                </CardWrapper>
+              )}
+            </>
+          )}
         </CardWrapper>
       </Section>
     </Container>
