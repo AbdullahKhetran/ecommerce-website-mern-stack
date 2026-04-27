39860a28547c86e08509cf2a2233b6a9bc709861
Abdullah Khetran <khetranabdullah@gmail.com>
2026-04-28 01:03:50 +0500

    fixed typo

--- server/models/Orders.js
+++ server/models/Orders.js
@@ -24,7 +24,7 @@ const OrdersScehema = new mongoose.Schema(
             type: String,
             default: "",
         },
-        address: {
+        status: {
             type: String,
             default: "Payment Done",
         },        
