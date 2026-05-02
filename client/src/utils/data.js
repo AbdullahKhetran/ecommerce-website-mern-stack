a45281debd9f501fa876495ddadfea839154c6b1
Abdullah Khetran <khetranabdullah@gmail.com>
2026-05-02 11:13:10 +0500

    updated url of categories image

--- client/src/utils/data.js
+++ client/src/utils/data.js
@@ -1,26 +1,26 @@
 export const category = [
   {
-    img: "https://plus.unsplash.com/premium_photo-1690366910345-5807bf328585?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    img: "https://res.cloudinary.com/dbjwc6uav/image/upload/v1777701951/ecom-mern/categories/casual-wear_jp1bz4.avif",
     name: "Casual Wear",
     off: "20-40% OFF",
   },
   {
-    img: "https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/24439364/2023/8/10/a157968c-bc17-4fa8-b805-31f930c47b381691681709790MenBlackSolidSlimFitFormalBlazer1.jpg",
+    img: "https://res.cloudinary.com/dbjwc6uav/image/upload/v1777701951/ecom-mern/categories/formal-wear_mmwnhx.jpg",
     name: "Formal Wear",
     off: "10-20% OFF",
   },
   {
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqR8nb8uO-HERwcu2eIWBFaSY7DC_KaX19hvBNApYgTgacS1Hhhx8WJOY_-jacJnwzXqY&usqp=CAU",
+    img: "https://res.cloudinary.com/dbjwc6uav/image/upload/v1777701953/ecom-mern/categories/winter-wear_bkfyuy.jpg",
     name: "Winter Wear",
     off: "20-40% OFF",
   },
   {
-    img: "https://images.bewakoof.com/utter/content/2484/content_western_dress_3.jpg",
+    img: "https://res.cloudinary.com/dbjwc6uav/image/upload/v1777701952/ecom-mern/categories/western-wear_e5msqq.jpg",
     name: "Western Wear",
     off: "30-40% OFF",
   },
   {
-    img: "https://plus.unsplash.com/premium_photo-1673757106128-eaab1a42ca49?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZXRobmljJTIwd2VhcnxlbnwwfHwwfHx8MA%3D%3D",
+    img: "https://res.cloudinary.com/dbjwc6uav/image/upload/v1777701951/ecom-mern/categories/ethnic-wear_h0hleh.avif",
     name: "Ethnic Wear",
     off: "10-40% OFF",
   },
